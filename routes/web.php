564ca82f98ca29dcd525e1585708753d43ecb4e9
santyloaiza74 <santyloaiza74@gmail.com>
2024-04-15 16:43:28 -0500

    manejo de rutas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('cursos',function () {
+    return view('cursos.index');
+});
+Route::get('cursos/create', function () {
+    return 'Formulario para crear un curso';
+});
+Route::get('cursos/{curso}',function ($curso) {
+    return 'bienvnido al curso: ' . $curso;
+});
